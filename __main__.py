825309f5ca895e4a2faeb4ace3eaa87216eeb7e7
Richard Brantley <brantleyr@gmail.com>
2017-05-19 14:53:39 -0400

    renaming insights-client egg to insights-core egg

--- __main__.py
+++ __main__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
-from insights_client.__init__ import main
+from insights_core.__init__ import main
 main()
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-	name = 'insights_client',
+	name = 'insights_core',
 	version = '3.0',
     author="Richard Brantley <rbrantle@redhat.com>",
     author_email="rbrantle@redhat.com",
     license="GPL",
 	packages = find_packages(),
-	entry_points={'console_scripts': ['insights-client = insights_client:main']},
+	entry_points={'console_scripts': ['insights-core = insights_core:main']},
 	py_modules=['__main__'],
 	include_package_data=True
 	)
