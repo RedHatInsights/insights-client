285523bc780bd55fc0a5258a30f31653a4215444
Richard Brantley <brantleyr@gmail.com>
2017-05-23 13:35:30 -0400

    adding basic options parser, testing passthrough from wrapper to egg

--- insights_core/__init__.py
+++ insights_core/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import logging
+import optparse
 
 __author__ = 'Richard Brantley <rbrantle@redhat.com>'
 LOG_FORMAT = ("%(asctime)s %(levelname)s %(message)s")
@@ -11,6 +12,34 @@ def main():
 	'''
 	Main entry point
 	'''
+
+	parser = optparse.OptionParser()
+	parser.add_option('--version','-v',
+                      help="Display version",
+                      action="store_true",
+                      dest="show_version",
+                      default=False)
+	parser.add_option('--verbose','-V',
+                      help="Run verbosely",
+                      action="store_true",
+                      dest="verbose",
+                      default=False)
+	parser.add_option('--nogpg','-G',
+                      help=optparse.SUPPRESS_HELP,
+                      action="store_true",
+                      dest="nogpg",
+                      default=False)
+	parser.add_option('--devmode',
+                      help=optparse.SUPPRESS_HELP,
+                      action="store_true",
+                      dest="devmode",
+                      default=False)
+	options, args = parser.parse_args()
+
+	if options.show_version:
+		print '3.X.X-X'
+		sys.exit(0)
+
 	print "Running Insights Core Egg"
 	sys.exit(0)
 
