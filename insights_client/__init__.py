f6851251b11e36808e570dd3dd9eb3bd6975b23c
Richard Brantley <brantleyr@gmail.com>
2017-07-14 11:43:40 -0400

    get rid of constants, feed core to client

--- insights_client/__init__.py
+++ insights_client/__init__.py
@@ -6,23 +6,24 @@
 import os
 import sys
 import logging
-from constants import InsightsConstants as constants
+from insights.client.constants import InsightsConstants as constants
+from insights.client import run
 
 __author__ = 'Richard Brantley <rbrantle@redhat.com>, Jeremy Crafts <jcrafts@redhat.com>, Dan Varga <dvarga@redhat.com>'
 
-LOG_FORMAT = ("%(asctime)s %(levelname)s %(message)s")
-APP_NAME = constants.app_name
-logger = logging.getLogger(APP_NAME)
 
 def _main():
     """
     Main entry point
     """
+
+    # Require Root to run
     if os.geteuid() is not 0:
         sys.exit("Red Hat Insights must be run as root")
 
-    print "Running Inights Client"
-    sys.exit(0)
+    # Instantiate Client API
+    sys.exit(run())
+
 
 if __name__ == '__main__':
     _main()
