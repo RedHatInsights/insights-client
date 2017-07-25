2cf97a2bdc872c3b1283d480687189535d014607
Jesse Jaggars <jjaggars@redhat.com>
2017-07-25 07:15:46 -0500

    tweaks to process calling; adding logging

--- insights_client/__init__.py
+++ insights_client/__init__.py
@@ -5,6 +5,9 @@
 """
 import sys
 import subprocess
+import logging
+
+logging.basicConfig()
 
 __author__ = 'Richard Brantley <rbrantle@redhat.com>, Jeremy Crafts <jcrafts@redhat.com>, Dan Varga <dvarga@redhat.com>'
 
@@ -15,19 +18,24 @@ EGGS = [
 ]
 
 
-def go(phase, eggs):
+def go(phase, eggs, inp=None):
     """
     Call the run script for the given phase.  If the phase succeeds returns the
     index of the egg that succeeded to be used in the next phase.
     """
     insights_command = ["insights-client-run"] + sys.argv[1:]
-    for idx, egg in enumerate(eggs):
-        return_code = subprocess.call(insights_command, env={
+    for egg in eggs:
+        logging.debug("Attempting %s with %s", phase, egg)
+        process = subprocess.Popen(insights_command, stdout=subprocess.PIPE, stdin=inp, env={
             "INSIGHTS_PHASE": str(phase),
             "PYTHONPATH": str(egg)
         })
-        if return_code == 0:
-            return idx
+        stdout, stderr = process.communicate(inp)
+        if stderr:
+            logging.error("%s failed with: %s", phase, stderr)
+        if process.return_code == 0:
+            logging.info("%s completed with: %s", stdout)
+            return stdout
 
 
 def _main():
@@ -38,10 +46,9 @@ def _main():
     """
 
     go('update', EGGS[1:])
-
-    idx = go('collect', EGGS)
-    if idx is not None:
-        go('upload', EGGS[idx:])
+    response = go('collect', EGGS)
+    if response is not None:
+        go('upload', EGGS, response)
 
 if __name__ == '__main__':
     _main()
