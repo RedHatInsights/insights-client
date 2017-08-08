7db0bc791a8d78cb0f5e5cd76929d053f579beac
Richard Brantley <brantleyr@gmail.com>
2017-08-08 09:39:19 -0400

    don't require root

--- insights_client/run.py
+++ insights_client/run.py
@@ -7,8 +7,5 @@ try:
 except ImportError:
 	sys.exit("Error importing insights.client for %s as %s" % (os.environ["INSIGHTS_PHASE"], os.environ["PYTHONPATH"]))
 
-if os.geteuid() is not 0:
-    sys.exit("Red Hat Insights must be run as root")
-else:
-    phase = getattr(insights.client, os.environ["INSIGHTS_PHASE"])
-    sys.exit(phase())
+phase = getattr(insights.client, os.environ["INSIGHTS_PHASE"])
+sys.exit(phase())
