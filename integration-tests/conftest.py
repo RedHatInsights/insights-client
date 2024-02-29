c2a3540f8a9712884a08079e8b17d01638396a5e
root <root@dell-per740-68-vm-03.lab.eng.pek2.redhat.com>
2024-02-28 21:30:49 -0500

    tests: adapt registration for Satellite
    When registering against a Satellite server, install the
    katello-ca-consumer RPM package, so the right CA certificates for that
    Satellite are available for subscription-manager.
    
    Also modify the registration to use activation keys if configured,
    so they can be used instead of username & password.
    
     On branch qianzhan_dev
     Changes to be committed:
    	modified:   conftest.py
    
     Author:    QianqianZhang <qianzhan@redhat.com>

--- integration-tests/conftest.py
+++ integration-tests/conftest.py
@@ -1,10 +1,36 @@
 import pytest
+import subprocess
 
 
 @pytest.fixture(scope="session")
-def register_subman(external_candlepin, subman_session, test_config):
-    subman_session.register(
-        username=test_config.get("candlepin", "username"),
-        password=test_config.get("candlepin", "password"),
-    )
+def install_katello_rpm(test_config):
+    if "satellite" in test_config.environment:
+        # install katello rpm before register system against Satellite
+        satellite_hostname = test_config.get("candlepin", "host")
+        cmd = [
+            "rpm",
+            "-Uvh",
+            "http://%s/pub/katello-ca-consumer-latest.noarch.rpm" % satellite_hostname,
+        ]
+        subprocess.check_call(cmd)
+    yield
+    if "satellite" in test_config.environment:
+        cmd = "rpm -qa 'katello-ca-consumer*' | xargs rpm -e"
+        subprocess.check_call(cmd, shell=True)
+
+
+@pytest.fixture(scope="session")
+def register_subman(
+    external_candlepin, subman_session, test_config, install_katello_rpm
+):
+    if "satellite" in test_config.environment:
+        subman_session.register(
+            activationkey=test_config.get("candlepin", "activation_keys"),
+            org=test_config.get("candlepin", "org"),
+        )
+    else:
+        subman_session.register(
+            username=test_config.get("candlepin", "username"),
+            password=test_config.get("candlepin", "password"),
+        )
     yield subman_session
