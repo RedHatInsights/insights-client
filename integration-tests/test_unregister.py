c3e5111b0f9dd1841c7deeb7c35d63ff2f89de52
Archana Pandey <arpandey@redhat.com>
2024-03-06 12:57:40 +0530

    Test : Added tests for insights-client --unregister option 	1- Verify --unregister operation on registered system 	2- Verify --unregister operation on already unregistered system        We need two seperate tests for test reporting.
    Signed-off-by: Archana Pandey <arpandey@redhat.com>

---
+++ integration-tests/test_unregister.py
@@ -0,0 +1,45 @@
+import pytest
+
+pytestmark = pytest.mark.usefixtures("register_subman")
+
+
+def test_unregister(insights_client):
+    """Test insights-client --unregister after registering it
+
+    If the unregistration is successful, `unregister` method returns True
+    """
+    insights_client.register()
+    assert insights_client.is_registered
+
+    unregistration_status = insights_client.run("--unregister")
+    assert (
+        "Successfully unregistered from the Red Hat Insights Service"
+        in unregistration_status.stdout
+    )
+    assert not insights_client.is_registered
+
+
+def test_unregister_twice(insights_client):
+    """Test insights-client --unregister on unregistered system
+
+    If the unregistration is successful, `--unregister` returns True on first attempt and
+    false on subsequent attempts.
+    """
+    insights_client.register()
+    assert insights_client.is_registered
+
+    # unregister once
+    unregistration_status = insights_client.run("--unregister")
+    assert not insights_client.is_registered
+    assert (
+        "Successfully unregistered from the Red Hat Insights Service"
+        in unregistration_status.stdout
+    )
+    # unregister twice
+    unregistration_status = insights_client.run("--unregister", check=False)
+    assert not insights_client.is_registered
+    assert unregistration_status.returncode == 1
+    assert (
+        "This host is not registered, unregistration is not applicable."
+        in unregistration_status.stdout
+    )
