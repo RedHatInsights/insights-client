87bbbc5e386cf53a0465685df092d08109470abc
QianqianZhang <qianzhan@redhat.com>
2024-03-28 00:40:49 -0400

    Move test_obfuscation.py:test_obfuscation to test_redaction.py:test_redaction_on_pattern_hostname

--- integration-tests/test_redaction.py
+++ integration-tests/test_redaction.py
@@ -1,4 +1,5 @@
 import pytest
+import subprocess
 from time import sleep
 import re
 import tarfile
@@ -11,6 +12,7 @@ pytestmark = pytest.mark.usefixtures("register_subman")
 TEST_CMD = ["uptime", "lsmod"]
 TEST_FILE = ["/proc/cpuinfo", "/proc/meminfo", "/etc/yum.conf", "/etc/os-release"]
 FILE_REDACTION_FILE = "/etc/insights-client/file-redaction.yaml"
+CONTENT_REDACTION_FILE = "/etc/insights-client/file-content-redaction.yaml"
 
 
 @pytest.mark.parametrize("not_removed_command", TEST_CMD)
@@ -35,7 +37,7 @@ def test_redaction_on_cmd(insights_client, removed_command):
     so related commands should not exist in collection archive.
     """
     try:
-        configure_redaction("command", removed_command)
+        configure_file_redaction("command", removed_command)
         insights_client.register()
         sleep(5)
         assert insights_client.is_registered
@@ -66,7 +68,7 @@ def test_redaction_on_file(insights_client, removed_file):
     so related files should not exist in collection archive.
     """
     try:
-        configure_redaction("file", removed_file)
+        configure_file_redaction("file", removed_file)
         insights_client.register()
         sleep(5)
         assert insights_client.is_registered
@@ -75,7 +77,58 @@ def test_redaction_on_file(insights_client, removed_file):
         os.remove(FILE_REDACTION_FILE)
 
 
-def configure_redaction(redaction_type, redaction_string):
+def test_redaction_on_pattern_hostname(insights_client, tmp_path):
+    """
+    Test if the satellite hostname is obfuscated
+    Related to https://issues.redhat.com/browse/RHEL-2471
+
+    """
+    # Register system to insights
+    insights_client.register()
+    # sleep(5)
+    # assert insights_client.is_registered
+
+    # Record the current hostname
+    with open("/etc/hostname", "r") as f:
+        previous_hostname = f.read().strip()
+
+    try:
+        # Set a new hostname
+        hostname_to_test = "insights-client-satellite"
+        set_hostname(hostname_to_test)
+        cmd = ["hostnamectl", "status"]
+        output = subprocess.check_output(cmd, universal_newlines=True)
+        assert hostname_to_test in output
+
+        # redaction on hostname:
+        configure_content_redaction(hostname_to_test)
+
+        # check the collection, if redaction pattern is filtered out
+        # collect data
+        archive_result = insights_client.run("--no-upload")
+
+        # extract the collected tar file
+        m = re.search("^Archive saved at (.+)$", archive_result.stdout, re.MULTILINE)
+        assert m
+        archive_location = m.group(1)
+        cmd = ["tar", "-vxzf", archive_location, "-C", tmp_path]
+        subprocess.check_call(cmd)
+
+        # the redaction data should not locate in the collected files
+        archive_extract = os.path.basename(archive_location).split(".")[0]
+        path_to_check = tmp_path / archive_extract
+        cmd = ["grep", "-rnw", ".", "-e", hostname_to_test]
+        # should not find the obfuscate pattern, the return code should not zero.
+        ret = subprocess.run(cmd, cwd=path_to_check)
+        assert ret.returncode == 1
+    finally:
+        # Recover the previous hostname for the system
+        set_hostname(previous_hostname)
+        # Remove all data redaction
+        os.remove(CONTENT_REDACTION_FILE)
+
+
+def configure_file_redaction(redaction_type, redaction_string):
     if redaction_type == "command":
         file_content = {"commands": [redaction_string]}
     elif redaction_type == "file":
@@ -96,3 +149,15 @@ def check_archive(insights_client, tested):
         ]
     logging.info(check_tested)
     return check_tested
+
+
+def configure_content_redaction(pattern_string):
+    file_content = {"patterns": {"regex": [pattern_string]}}
+    with open(CONTENT_REDACTION_FILE, "w") as f:
+        yaml.dump(file_content, f, default_flow_style=False)
+    os.chmod(CONTENT_REDACTION_FILE, 0o600)
+
+
+def set_hostname(hostname_to_set):
+    cmd = ["hostnamectl", "set-hostname", hostname_to_set]
+    subprocess.check_call(cmd)
