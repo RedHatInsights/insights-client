9b45b27b7a32572e962741b8c855abac6ede9f94
zpetrace <zpetrace@redhat.com>
2024-04-15 15:13:32 +0200

    feat(test): Migrating no_upload_test.py upstream

--- integration-tests/test_client_options.py
+++ integration-tests/test_client_options.py
@@ -1,7 +1,11 @@
 import pytest
 import conftest
+import glob
+
 
 pytestmark = pytest.mark.usefixtures("register_subman")
+
+ARCHIVE_CACHE_DIRECTORY = "/var/cache/insights-client"
 
 
 def test_set_ansible_host_info(insights_client):
@@ -17,3 +21,22 @@ def test_set_ansible_host_info(insights_client):
     ret = insights_client.run("--ansible-host=foo.example.com", check=False)
     assert "Could not update Ansible hostname" not in ret.stdout
     assert ret.returncode == 0
+
+
+def test_no_upload(insights_client):
+    """Verify no traceback is returned when running '--no-upload' command
+    and the archive is created and saved on the system
+    """
+    archive_saved = "Archive saved at"
+    upload_message = "Successfully uploaded report"
+
+    insights_client.register()
+
+    archive_file_before = glob.glob(f"{ARCHIVE_CACHE_DIRECTORY}/*.tar.gz")
+
+    no_upload_output = insights_client.run("--no-upload")
+    assert archive_saved in no_upload_output.stdout
+    assert upload_message not in no_upload_output.stdout
+
+    archive_file_after = glob.glob(f"{ARCHIVE_CACHE_DIRECTORY}/*.tar.gz")
+    assert len(archive_file_after) > len(archive_file_before)
