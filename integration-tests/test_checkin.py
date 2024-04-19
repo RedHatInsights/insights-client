766e6e51774eb82addc78aa8940962f1596d53dc
Archana Pandey <arpandey@redhat.com>
2024-04-19 17:54:38 +0530

    Test : 	Added test for ultra-light checkin, --checkin operation 	Added test for --checkin on unregistered sytstem
    Signed-off-by: Archana Pandey <arpandey@redhat.com>

---
+++ integration-tests/test_checkin.py
@@ -0,0 +1,46 @@
+import contextlib
+import json
+
+import pytest
+
+pytestmark = pytest.mark.usefixtures("register_subman")
+
+HOST_DETAILS = "/var/lib/insights/host-details.json"
+
+
+def test_ultralight_checkin(insights_client):
+    """test --checkin
+    Sends nothing but canonical facts to update the host stale and updated timestamp
+    """
+    insights_client.register()
+    # Performing check-results operation provides latest host data in host-details.json
+    insights_client.run("--check-results")
+    with open(HOST_DETAILS, "r") as data_file:
+        data = json.load(data_file)
+        stale_ts_before_checkin = data["results"][0]["stale_timestamp"]
+        updated_ts_before_checkin = data["results"][0]["updated"]
+
+    # Performing an ultra light checkin
+    insights_client.run("--checkin")
+    insights_client.run("--check-results")
+
+    with open(HOST_DETAILS, "r") as data_file:
+        data = json.load(data_file)
+        stale_ts_after_checkin = data["results"][0]["stale_timestamp"]
+        updated_ts_after_checkin = data["results"][0]["updated"]
+
+    assert stale_ts_after_checkin > stale_ts_before_checkin
+    assert updated_ts_after_checkin > updated_ts_before_checkin
+
+
+def test_client_checkin_unregistered(insights_client):
+    """Call insights client checkin with unregistered client."""
+    with contextlib.suppress(Exception):
+        insights_client.unregister()
+    assert not insights_client.is_registered
+
+    checkin_result = insights_client.run("--checkin", check=False)
+    assert checkin_result.returncode == 1
+    assert (
+        "Error: failed to find host with matching machine-id" in checkin_result.stdout
+    )
