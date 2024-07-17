5689a2a117f8209f05672d4f554f31c050ec97ca
James Molet <jmolet@redhat.com>
2024-07-17 12:05:03 -0400

    feat(test):  Allow CI to install insights-core
    Install insights-core if a branch of it is specified.
    This will sign it with a test gpg key and place the egg
    in the proper location

--- systemtest/tests/integration/test.sh
+++ systemtest/tests/integration/test.sh
@@ -14,7 +14,10 @@ cd ../../../
 rpm -q insights-client || ./systemtest/guest-setup.sh
 
 dnf --setopt install_weak_deps=False install -y \
-  podman git-core python3-pip python3-pytest logrotate bzip2
+  podman git-core python3-pip python3-pytest logrotate bzip2 zip
+
+# If this is an insightsCore PR build and sign the new egg.
+[ -z "${insightsCoreBranch+x}" ] || ./systemtest/insights-core-setup.sh
 
 python3 -m venv venv
 # shellcheck disable=SC1091
