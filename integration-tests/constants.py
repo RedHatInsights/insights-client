42919eba59c6e3c7f9500a62a618822c0d016cf0
QianqianZhang <qianzhan@redhat.com>
2024-09-26 06:56:35 -0400

    test: add insights-client test case for tags

--- integration-tests/constants.py
+++ integration-tests/constants.py
@@ -1,2 +1,5 @@
+import pathlib
+
 HOST_DETAILS: str = "/var/lib/insights/host-details.json"
 MACHINE_ID_FILE: str = "/etc/insights-client/machine-id"
+TAGS_FILE = pathlib.Path("/etc/insights-client/tags.yaml")
