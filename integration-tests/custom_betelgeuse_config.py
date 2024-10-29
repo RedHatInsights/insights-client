141c5c1ebdcd51606157ee520fb2ae17d468f31d
zpetrace <zpetrace@redhat.com>
2024-10-29 11:07:54 +0100

    fix(test): Changing the custom fields to enable upload to Polarion (CCT-616)
    Polarion already has a built-in field for Requirement so when we added the
    custom field for Polarion, the upload was failing due to a conflict. I
    have therefore removed the custom field from the betelgeuse setting as
    Betelgeuse can match the requirement field already built-in into
    Polarion and there was no need for it to have it as a custom field

--- integration-tests/custom_betelgeuse_config.py
+++ integration-tests/custom_betelgeuse_config.py
@@ -2,12 +2,10 @@ from betelgeuse import default_config
 
 TESTCASE_CUSTOM_FIELDS = default_config.TESTCASE_CUSTOM_FIELDS + (
     "casecomponent",
-    "requirement",
     "subsystemteam",
     "reference",
 )
 
 DEFAULT_CASECOMPONENT_VALUE = ""
-DEFAULT_REQUIREMENT_VALUE = ""
 DEFAULT_SUBSYSTEMTEAM_VALUE = ""
 DEFAULT_REFERENCE_VALUE = ""
