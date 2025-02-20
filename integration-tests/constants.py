d5dd5d92ed53a21ccd6403e8da50731eafd7b88f
pkoprda <pkoprda@redhat.com>
2025-02-20 16:44:58 +0100

    test: Unregistration should be always explicit
    * Card ID: CCT-1122
    
    Previously, certain commands (e.g., `--status`) could unintentionally
    trigger system unregistration if the host was missing from the
    Inventory. This change adds the integration test which verifies that
    system unregistration does not happen implicitly.

--- integration-tests/constants.py
+++ integration-tests/constants.py
@@ -1,5 +1,7 @@
 import pathlib
 
 HOST_DETAILS: str = "/var/lib/insights/host-details.json"
+REGISTERED_FILE: str = "/etc/insights-client/.registered"
+UNREGISTERED_FILE: str = "/etc/insights-client/.unregistered"
 MACHINE_ID_FILE: str = "/etc/insights-client/machine-id"
 TAGS_FILE = pathlib.Path("/etc/insights-client/tags.yaml")
