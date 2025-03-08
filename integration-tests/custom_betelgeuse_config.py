2f39a00f895e08bc0ce176a14856deae5e23ded6
zpetrace <zpetrace@redhat.com>
2025-03-08 12:21:04 +0100

    fix(test): Ensure that tests are part of evidence traceability matrix. Based on CCT-452 I have added the neccessary docstrings to the test classes and edited the testimony.yaml and custom betelgeuse config to accomodate these changes.
    (cherry picked from commit 9f0025309ffd82d279e0b6613a0c5fbdf51a8e90)

--- integration-tests/custom_betelgeuse_config.py
+++ integration-tests/custom_betelgeuse_config.py
@@ -4,8 +4,14 @@ TESTCASE_CUSTOM_FIELDS = default_config.TESTCASE_CUSTOM_FIELDS + (
     "casecomponent",
     "subsystemteam",
     "reference",
+    "polarionincludeskipped",
+    "polarionlookupmethod",
+    "polarionprojectid",
 )
 
 DEFAULT_CASECOMPONENT_VALUE = ""
 DEFAULT_SUBSYSTEMTEAM_VALUE = ""
 DEFAULT_REFERENCE_VALUE = ""
+POLARION_INCLUDE_SKIPED = ""
+POLARION_LOOKUP_METHOD = ""
+POLARION_PROJECT_ID = ""
