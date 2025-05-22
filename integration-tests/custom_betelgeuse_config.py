b25db5ec26099eb9873e1c4e533692dd2e74b968
zpetrace <zpetrace@redhat.com>
2025-05-22 13:04:47 +0200

    fix(test): Fixing betelgeuse - polarion docstrings
    The fields we were using were Betelgeuse pre-filled fields, now I
    have changed the fields into what Polarion needs for a
    successful upload with the necessary fields included.
    This is a part of work for CCT-1090 for el8.
    
    (cherry picked from commit 9924151c754e3d5e26e580e22b42d21b9fc4b217)

--- integration-tests/custom_betelgeuse_config.py
+++ integration-tests/custom_betelgeuse_config.py
@@ -1,16 +1,16 @@
 from betelgeuse import default_config
 
 TESTCASE_CUSTOM_FIELDS = default_config.TESTCASE_CUSTOM_FIELDS + (
-    "casecomponent",
-    "subsystemteam",
+    "component",
+    "poolteam",
     "reference",
     "polarionincludeskipped",
     "polarionlookupmethod",
     "polarionprojectid",
 )
 
-DEFAULT_CASECOMPONENT_VALUE = ""
-DEFAULT_SUBSYSTEMTEAM_VALUE = ""
+DEFAULT_COMPONENT_VALUE = ""
+DEFAULT_POOLTEAM_VALUE = ""
 DEFAULT_REFERENCE_VALUE = ""
 POLARION_INCLUDE_SKIPED = ""
 POLARION_LOOKUP_METHOD = ""
