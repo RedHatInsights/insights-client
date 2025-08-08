3efc0cb84653655a3f61a5a10e0963268726b7fb
Peter Schrimpel <pschrimp@redhat.com>
2025-08-08 12:21:50 +0200

    feat(test): Add PCP Zeroconf integration test
    * CardID: CCT-1463
    
    * Added integration tests for PCP Zeroconf package and pmlogger service
    for RHEL 8.

--- integration-tests/constants.py
+++ integration-tests/constants.py
@@ -6,3 +6,4 @@ UNREGISTERED_FILE: str = "/etc/insights-client/.unregistered"
 MACHINE_ID_FILE: str = "/etc/insights-client/machine-id"
 TAGS_FILE = pathlib.Path("/etc/insights-client/tags.yaml")
 INSIGHTS_CLIENT_LOG_FILE = "/var/log/insights-client/insights-client.log"
+CONFIG_FILE = "/etc/insights-client/insights-client.conf"
