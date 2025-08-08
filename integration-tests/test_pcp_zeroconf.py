e9e30b51f3fa1a33bbb64f360b451b67037e4c8c
Peter Schrimpel <pschrimp@redhat.com>
2025-08-08 14:25:27 +0200

    a

--- integration-tests/test_pcp_zeroconf.py
+++ integration-tests/test_pcp_zeroconf.py
@@ -17,38 +17,39 @@ from constants import CONFIG_FILE
 PACKAGE = "pcp-zeroconf"
 SERVICE = "pmlogger"
 
+pytestmark = pytest.mark.usefixtures("register_subman")
 
 @pytest.mark.tier1
 def test_pcp_zeroconf_install():
     install = subprocess.run(
-        ["dnf", "install", "-y", PACKAGE], capture_output=True, text=True
+        ["dnf", "install", "-y", PACKAGE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
     )
-    assert install.returncode == 0, f"{PACKAGE} was not installed"
+    assert install.returncode == 0, f"{PACKAGE} was not installed: {install.stderr}"
 
     pmlogger_status = subprocess.run(
-        [SERVICE], capture_output=True, text=True
+        [SERVICE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
     )
     service_not_found = f"{SERVICE}: command not found"
     assert not service_not_found in pmlogger_status.stderr, service_not_found
 
     grep_result = subprocess.run(
-        ["pcp", "|", "grep", SERVICE], capture_output=True, text=True, shell=True
+        ["pcp", "|", "grep", SERVICE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True
     )
     assert grep_result.returncode == 0, f"{SERVICE} is not running"
 
-    cat_result = subprocess.run(["cat", CONFIG_FILE], capture_output=True, text=True)
+    cat_result = subprocess.run(["cat", CONFIG_FILE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     assert "auto_update=True" in cat_result.stdout
 
 
 @pytest.mark.tier1
 def test_pmlogger_running_and_metrics_exist():
     status_result = subprocess.run(
-        ["systemctl", "is-active", SERVICE], capture_output=True, text=True
+        ["systemctl", "is-active", SERVICE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
     )
     if status_result.stdout.strip() != "active":
         subprocess.run(["systemctl", "start", SERVICE], check=True)
         status_result = subprocess.run(
-            ["systemctl", "is-active", SERVICE], capture_output=True, text=True
+            ["systemctl", "is-active", SERVICE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
         )
     assert status_result.stdout.strip() == "active", f"{SERVICE} is not active"
 
@@ -65,7 +66,7 @@ def test_cleanup_pmlogger_and_pcp():
 
     # Stop pmlogger service
     stop_result = subprocess.run(
-        ["systemctl", "stop", SERVICE], capture_output=True, text=True
+        ["systemctl", "stop", SERVICE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
     )
     assert (
         stop_result.returncode == 0
@@ -73,7 +74,7 @@ def test_cleanup_pmlogger_and_pcp():
 
     # Verify pmlogger is inactive
     status_result = subprocess.run(
-        ["systemctl", "is-active", SERVICE], capture_output=True, text=True
+        ["systemctl", "is-active", SERVICE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
     )
     assert (
         status_result.stdout.strip() != "active"
@@ -81,7 +82,7 @@ def test_cleanup_pmlogger_and_pcp():
 
     # Uninstall pcp-zeroconf package
     uninstall_result = subprocess.run(
-        ["dnf", "remove", "-y", PACKAGE], capture_output=True, text=True
+        ["dnf", "remove", "-y", PACKAGE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
     )
     assert (
         uninstall_result.returncode == 0
