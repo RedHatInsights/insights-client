0e6ebf8613ace9a2d42296b10679944c0561d476
zpetrace <zpetrace@redhat.com>
2025-08-13 15:01:18 +0200

    fix(test): Remove fields now provided by Jenkins pipeline
    These fields are now injected via the Jenkins pipeline,
    so we don't need to define them in the integration tests and
    related config. This removes duplication and avoids drift between
    CI and the test suite.
    
    (cherry picked from commit 5cc11ab27b2a88b80356f6ed1e18e22c29cb9dba)

--- integration-tests/custom_betelgeuse_config.py
+++ integration-tests/custom_betelgeuse_config.py
@@ -4,14 +4,8 @@ TESTCASE_CUSTOM_FIELDS = default_config.TESTCASE_CUSTOM_FIELDS + (
     "casecomponent",
     "subsystemteam",
     "reference",
-    "polarionincludeskipped",
-    "polarionlookupmethod",
-    "polarionprojectid",
 )
 
 DEFAULT_COMPONENT_VALUE = ""
 DEFAULT_POOLTEAM_VALUE = ""
 DEFAULT_REFERENCE_VALUE = ""
-POLARION_INCLUDE_SKIPED = ""
-POLARION_LOOKUP_METHOD = ""
-POLARION_PROJECT_ID = ""
