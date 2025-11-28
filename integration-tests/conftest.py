3dbf6eec6a69ab8e9c196b2367038dd9dc15e61c
Pavel Holica <pholica@redhat.com>
2025-11-28 12:29:06 +0100

    Workaround for missing output from insights_core
    co-authored by black, the formatting is not mine.
    Good job, turning 3 simple commands to 18 lines...

--- integration-tests/conftest.py
+++ integration-tests/conftest.py
@@ -1,5 +1,8 @@
+import os
+
 import pytest
 import subprocess
+import tempfile
 import logging
 
 logger = logging.getLogger(__name__)
@@ -37,6 +40,62 @@ def register_subman(
             password=test_config.get("candlepin", "password"),
         )
     yield subman_session
+
+
+@pytest.fixture(scope="session", autouse=True)
+def insights_core_workaround():
+    """
+    Workaround for https://issues.redhat.com/browse/RHINENG-21918
+    When insights-client is started by a user (running shell in unconfined mode)
+    and the insights-client is run in insights_client_t SELinux context type
+    (effectively switching the next python process that executes the insights_core
+    code to insights_core_t SELinux context type), printing to PTY or pipe
+    (through inherited file descriptor) that belongs to the user is not allowed.
+    This Workaround fixture allows these actions in the active SELinux policy.
+    """
+    policy = """module core_output 1.0;
+
+require {
+  type insights_core_t;
+  type unconfined_t;
+  type user_devpts_t;
+  class fifo_file write;
+  class chr_file { ioctl read write };
+}
+
+#============= insights_core_t ==============
+allow insights_core_t unconfined_t:fifo_file write;
+allow insights_core_t user_devpts_t:chr_file { ioctl read write };
+"""
+    origdir = os.getcwd()
+    with tempfile.TemporaryDirectory() as tempdirname:
+        try:
+            os.chdir(tempdirname)
+            with open("core_output.te", "wt") as selinux_file:  # codespell:ignore te
+                selinux_file.write(policy)
+            subprocess.run(
+                [
+                    "checkmodule",
+                    "-M",
+                    "-m",
+                    "-o",
+                    "core_output.mod",
+                    "core_output.te",  # codespell:ignore te
+                ],
+                check=True,
+            )
+            subprocess.run(
+                ["semodule_package", "-o", "core_output.pp", "-m", "core_output.mod"],
+                check=True,
+            )
+            subprocess.run(
+                ["semodule", "-i", "core_output.pp"],
+                check=True,
+            )
+        finally:
+            os.chdir(origdir)
+    yield
+    subprocess.run(["semodule", "-r", "core_output"], check=True)
 
 
 def check_is_bootc_system():
