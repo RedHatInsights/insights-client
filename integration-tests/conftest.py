4d5374c96a0e0505d3869fb68adfb74495451807
Pavel Holica <pholica@redhat.com>
2026-01-27 11:52:47 +0100

    workaround(tests): skip known AVCs
    Ref: https://issues.redhat.com/browse/RHEL-145608

--- integration-tests/conftest.py
+++ integration-tests/conftest.py
@@ -164,6 +164,41 @@ def wait_for_services_to_finish(services=None):
     logger.debug(datetime.datetime.now(), "Finished waiting for systemd services to finish")
 
 
+def add_known_avcs_to_skiplist(avc_checker):
+    avc_checker.skip_avc_entry_by_fields(
+        {
+            "subj": "system_u:system_r:insights_client_t:s0",
+            "syscall": "openat",
+            "permission": "write",
+            "obj": "unconfined_u:object_r:var_run_t:s0",
+        }
+    )  # Bug: https://issues.redhat.com/browse/RHEL-146687
+    avc_checker.skip_avc_entry_by_fields(
+        {
+            "subj": "system_u:system_r:insights_client_t:s0",
+            "syscall": "newfstatat",
+            "permission": "getattr",
+            "obj": "unconfined_u:object_r:var_run_t:s0",
+        }
+    )  # Bug: https://issues.redhat.com/browse/RHEL-146687
+    avc_checker.skip_avc_entry_by_fields(
+        {
+            "subj": "system_u:system_r:insights_client_t:s0",
+            "syscall": "openat",
+            "permission": "search",
+            "obj": "unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023",
+        }
+    )  # Bug: https://issues.redhat.com/browse/RHEL-145608
+    avc_checker.skip_avc_entry_by_fields(
+        {
+            "subj": "system_u:system_r:insights_client_t:s0",
+            "syscall": "fstat",
+            "permission": "getattr",
+            "obj": "unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023",
+        }
+    )  # Bug: https://issues.redhat.com/browse/RHEL-145608
+
+
 @pytest.fixture(autouse=True)
 def check_avcs(request):
     """
