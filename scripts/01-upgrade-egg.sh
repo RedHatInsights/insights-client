ab4aa8b4540650f8932b743529f0717a2e5beb6d
pkoprda <pkoprda@redhat.com>
2026-07-22 19:53:00 +0200

    chore: remove unused EGG_VERSION reference from egg upgrade script
    The EGG_VERSION file is no longer used; the egg is downloaded
    directly from console.redhat.com. Update README accordingly.
    
    Signed-off-by: pkoprda <pkoprda@redhat.com>

--- scripts/01-upgrade-egg.sh
+++ scripts/01-upgrade-egg.sh
@@ -16,7 +16,5 @@ for PROGRAM in "${PROGRAMS[@]}"; do
     fi
 done
 
-TAG=$(cat EGG_VERSION)
-
 curl --fail -L https://console.redhat.com/api/v1/static/release/insights-core.el10.egg --output data/rpm.egg
 curl --fail -L https://console.redhat.com/api/v1/static/release/insights-core.el10.egg.asc --output data/rpm.egg.asc
